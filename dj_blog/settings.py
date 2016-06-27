04ba58ba1b5780c0c772dfcb3bb4906b9ef7de98
linbinbin <lins01jp@gmail.com>
2016-06-27 15:25:23 +0900

    change for admin manager

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dj_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/west/'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
